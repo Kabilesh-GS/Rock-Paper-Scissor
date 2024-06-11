28fa728c7ed1f57001b97ee28b6c3a4872571e8d
Kabilesh-GS <kabileshgs@gmail.com>
2024-06-11 14:12:06 +0530

    Fixes / added local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,104 +1,114 @@
-let rock = document.getElementById("rockbtn");
-let paper = document.getElementById("paperbtn");
-let scissor = document.getElementById("scissorbtn");
 let yourch  = document.getElementById("yourchoice");
 let compch  = document.getElementById("compchoice");
-let reset = document.getElementById("resetbtn");
 let result = document.getElementById("resultbox");
 let yoursc = document.getElementById("yourscore");
 let compsc = document.getElementById("computerscore");
 let emoji = document.getElementById("emojibox");
-let score = {
+let score = JSON.parse(localStorage.getItem('scores')) || {
     yourscore: 0,
     compscore: 0
-}
-
-rock.onclick = function(){
-    let random = Math.round(Math.random() * 2);
-    if(random == 0){
-        yourch.innerHTML = "You : ✊";
-        result.textContent = "Draw";
-        compch.textContent = "✊ : Computer"
-        emoji.textContent = "🟰";
-    }
-    else if(random == 1){
-        yourch.innerHTML = "You : ✊";
-        result.textContent = "Computer Won!";
-        compch.textContent = "✋ : Computer";
-        emoji.textContent = "🤖";
-        score.compscore += 1;
-        compsc.textContent = `Computer : ${score.compscore}`;
-    }
-    else if(random == 2){
-        yourch.innerHTML = "You : ✊";
-        result.textContent = "You Won!";
-        compch.textContent = "✌️ : Computer";
-        emoji.textContent = "👦";
-        score.yourscore+=1;
-        yoursc.textContent = `You : ${score.yourscore}`;
-    }
-}
-paper.onclick = function(){
-    let random = Math.round(Math.random() * 2);
-    if(random == 0){
-        yourch.innerHTML = "You : ✋";
-        result.textContent = "You Won!";
-        compch.textContent = "✊ : Computer";
-        emoji.textContent = "👦";
-        score.yourscore+=1;
-        yoursc.textContent = `You : ${score.yourscore}`;
-    }
-    else if(random == 1){
-        yourch.innerHTML = "You : ✋";
-        result.textContent = "Draw";
-        compch.textContent = "✋ : Computer";
-        emoji.textContent = "🟰";
-    }
-    else if(random == 2){
-        yourch.innerHTML = "You : ✋";
-        result.textContent = "Computer Won!";
-        compch.textContent = "✌️ : Computer"
-        emoji.textContent = "🤖";
-        score.compscore+=1;
-        compsc.textContent = `Computer : ${score.compscore}`;
-    }
-}
-scissor.onclick = function(){
-    let random = Math.round(Math.random() * 2);
-    if(random == 0){
-        yourch.innerHTML = "You : ✌️";
-        result.textContent = "Computer Won";
-        compch.textContent = "✊ : Computer";
-        emoji.textContent = "🤖";
-        score.compscore+=1;
-        compsc.textContent = `Computer : ${score.compscore}`;
-    }
-    else if(random == 1){
-        yourch.innerHTML = "You : ✌️";
-        result.textContent = "You Won!";
-        compch.textContent = "✋ : Computer";
-        emoji.textContent = "👦";
-        score.yourscore+=1;
-        yoursc.textContent = `You : ${score.yourscore}`;
-    }
-    else if(random == 2){
-        yourch.innerHTML = "You : ✌️";
-        result.textContent = "Draw";
-        compch.textContent = "✌️ : Computer";
-        emoji.textContent = "🟰";
-    }
-}
-reset.onclick =  function(){
-    score.compscore=0;
-    score.yourscore=0;
-    compsc.textContent = `Computer : ${score.compscore}`;
-    yoursc.textContent = `You : ${score.yourscore}`;
-    result.innerHTML = "You or Computer, Let's see!";
-    emoji.innerHTML = "👦/🤖";
-    yourch.innerHTML = "You :";
-    compch.innerHTML = ": Computer";
 }
 
 new kursor({
     type: 1
 })
+
+function reset(){
+    score = {
+        yourscore: 0,
+        compscore: 0
+    }
+    localStorage.removeItem('scores');
+}
+
+function Computer(){
+    let random = Math.round(Math.random()*2);
+    let compchoise = '';
+    if(random == 0){
+        compchoise ='rock';
+    }
+    else if(random == 1){
+        compchoise ='paper';
+    }
+    else if(random == 2){
+        compchoise ='scissor';
+    }
+    return compchoise;
+}
+function Play(choice){
+    let comp = Computer();
+    if(choice == 'rock'){
+        if(comp == choice){
+            yourch.innerHTML = "You : ✊";
+            result.textContent = "Draw";
+            compch.textContent = "✊ : Computer"
+            emoji.textContent = "🟰";
+        }
+        else if(comp == 'paper'){
+            yourch.innerHTML = "You : ✊";
+            result.textContent = "Computer Won!";
+            compch.textContent = "✋ : Computer";
+            emoji.textContent = "🤖";
+            score.compscore += 1;
+            compsc.textContent = `Computer : ${score.compscore}`;
+        }
+        else if(comp == 'scissor'){
+            yourch.innerHTML = "You : ✊";
+            result.textContent = "You Won!";
+            compch.textContent = "✌️ : Computer";
+            emoji.textContent = "👦";
+            score.yourscore+=1;
+            yoursc.textContent = `You : ${score.yourscore}`;
+        }
+    }
+    if(choice == 'paper'){
+        if(comp == choice){
+            yourch.innerHTML = "You : ✋";
+            result.textContent = "Draw";
+            compch.textContent = "✋ : Computer";
+            emoji.textContent = "🟰";
+        }
+        if(comp == 'rock'){
+            yourch.innerHTML = "You : ✋";
+            result.textContent = "You Won!";
+            compch.textContent = "✊ : Computer";
+            emoji.textContent = "👦";
+            score.yourscore+=1;
+            yoursc.textContent = `You : ${score.yourscore}`;
+        }
+        if(comp == 'scissor'){
+            yourch.innerHTML = "You : ✋";
+            result.textContent = "Computer Won!";
+            compch.textContent = "✌️ : Computer"
+            emoji.textContent = "🤖";
+            score.compscore+=1;
+            compsc.textContent = `Computer : ${score.compscore}`;
+        }
+    }
+    if(choice == 'scissor'){
+        if(comp == choice){
+            yourch.innerHTML = "You : ✌️";
+            result.textContent = "Draw";
+            compch.textContent = "✌️ : Computer";
+            emoji.textContent = "🟰";
+        }
+        if(comp == 'rock'){
+            yourch.innerHTML = "You : ✌️";
+            result.textContent = "Computer Won";
+            compch.textContent = "✊ : Computer";
+            emoji.textContent = "🤖";
+            score.compscore+=1;
+            compsc.textContent = `Computer : ${score.compscore}`;
+        }
+        if(comp == 'paper'){
+            yourch.innerHTML = "You : ✌️";
+            result.textContent = "You Won!";
+            compch.textContent = "✋ : Computer";
+            emoji.textContent = "👦";
+            score.yourscore+=1;
+            yoursc.textContent = `You : ${score.yourscore}`;
+        }
+    }
+    let JSONscore = JSON.stringify(score);
+    localStorage.setItem('scores',JSONscore);
+}
